5f94f35d977a4531fc3058657f64445c996394e3
MohanrajElumalai <maybemohan8899@gmail.com>
2025-07-31 14:10:41 +0530

    python program7

---
+++ python program7.py
@@ -0,0 +1,8 @@
+print("CIRCLE PROGRAM")
+print("...................")
+a=int=(input("enter the r value:"))
+print("result")
+pc=2*22/7*7
+print("perimeter of circlue",pc)
+pr=22/7*7
+print("area of circlue",pr)
