c584f59665eaef2165cf07f54667adf83fe2b453
MohanrajElumalai <maybemohan8899@gmail.com>
2025-07-31 14:00:42 +0530

    PYTHON PROGRAM6

---
+++ python program6.ph.py
@@ -0,0 +1,14 @@
+print("square and rectangle")
+print(".......................")
+a=int(input("enter the A value:"))
+b=int(input("enter the B value:"))
+l=int(input("enter the L value:"))
+print("result")
+ps=4*a
+print("pertmeter of square:")
+AS=a*a
+print("area of square:",AS)
+pr=2*(l+b)
+print("permeter of rectangle:",pr)
+AR=l*b
+print("area of rectangle:",AS)
