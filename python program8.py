748a7e03155d0738b5f41138c8b72c60c481fd4e
MohanrajElumalai <maybemohan8899@gmail.com>
2025-08-06 15:58:52 +0530

    python program8

---
+++ python program8.py
@@ -0,0 +1,12 @@
+print("TRIANGLE PROGRAM")
+print("...................")
+a=(input("enter the a value:"))
+b=(input("enter the b value:"))
+c=i=(input("enter the c value:"))
+n=(input("enter the n value:"))
+print("result")
+pt=a+b+c
+print("perimeter of triangle",pt)
+at=1/2*b*n
+print("area of triangle",at)
+
