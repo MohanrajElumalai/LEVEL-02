0f09097de75db469549f77f80cde7690973b4fc6
MohanrajElumalai <maybemohan8899@gmail.com>
2025-08-06 16:20:35 +0530

    python program9

---
+++ python program9.py
@@ -0,0 +1,10 @@
+print("sum of number")
+print("-------------------")
+n=int(input("enter the n value"))
+print("result")
+sum=n*(n+1)/2
+print("sum:",sum)
+sumsquare=n*(n+1)*(2*n+1)/6
+print("sumsquare:",sumsquare)
+sumcube=(n*(n+1)/2)*(n*(n+1)/2)
+print("sumsquare:",sumcube)
