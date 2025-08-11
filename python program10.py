68b8189c2bf5dd1c5a098546256c7c9ed4fa7592
MohanrajElumalai <maybemohan8899@gmail.com>
2025-08-11 10:46:55 +0530

    python program10

---
+++ python program10.py
@@ -0,0 +1,14 @@
+print("EXPNENTION PROGRAM")
+print("................")
+x=int(input("enter the x value:"))
+y=int(input("enter the y value:"))
+z=int(input("enter the z value:"))
+print("result")
+x=x*2+y*2+z*2
+print("x value:",x)
+y=x*3+y*2+2+20
+print("y value:",y)
+z=x*2*y+y*2*z+z*2+10
+print("z value:",z)
+s=x*3*y*2*z
+print("s value:",s)
